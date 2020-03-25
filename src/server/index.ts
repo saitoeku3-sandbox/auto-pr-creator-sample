ef5b782904210a0f7c937fb8c963a7d12a345ac0
Tadao Iseki <saitoeku3@gmail.com>
2020-03-25 17:52:55 +0900

    fix: convert source codes to TypeScript

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,21 +1,20 @@
-const express = require('express')
-const consola = require('consola')
-const { Nuxt, Builder } = require('nuxt')
+import express from 'express'
+import consola from 'consola'
+import { Nuxt, Builder } from 'nuxt'
+import config from '../../nuxt.config'
+
 const app = express()
 
 // Import and Set Nuxt.js options
-const config = require('../nuxt.config.js')
-config.dev = process.env.NODE_ENV !== 'production'
+const isDev = process.env.NODE_ENV !== 'production'
 
-async function start () {
+const start = async () => {
   // Init Nuxt.js
   const nuxt = new Nuxt(config)
-
-  const { host, port } = nuxt.options.server
-
   await nuxt.ready()
+
   // Build only in dev mode
-  if (config.dev) {
+  if (isDev) {
     const builder = new Builder(nuxt)
     await builder.build()
   }
@@ -24,10 +23,12 @@ async function start () {
   app.use(nuxt.render)
 
   // Listen the server
+  const { host, port } = nuxt.options.server
   app.listen(port, host)
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
     badge: true
   })
 }
+
 start()
