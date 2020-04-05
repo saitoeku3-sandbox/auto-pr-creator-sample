0a7bfee2fd056c082f7ea755b3c49923389c0a1d
Tadao Iseki <saitoeku3@gmail.com>
2020-04-05 11:19:15 +0900

    Update

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -2,10 +2,12 @@ type Env = {
   GITHUB_OWNER: string
   GITHUB_REPO: string
   GITHUB_TOKEN: string
+  PORT: number
 }
 
 export const env: Env = {
   GITHUB_OWNER: process.env.GITHUB_OWNER || '',
   GITHUB_REPO: process.env.GITHUB_REPO || '',
   GITHUB_TOKEN: process.env.GITHUB_TOKEN || '',
+  PORT: Number(process.env.PORT) || 3000,
 }
